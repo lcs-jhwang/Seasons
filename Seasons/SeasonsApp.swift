1835696dc022dcd33c23f86145e2f90cb454bbec
Julien Hwang <jhwang@lcs.on.ca>
2024-12-10 19:35:59 -0500

    Initial Commit

---
+++ Seasons/SeasonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeasonsApp.swift
+//  Seasons
+//
+//  Created by Julien Hwang on 10/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct SeasonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
